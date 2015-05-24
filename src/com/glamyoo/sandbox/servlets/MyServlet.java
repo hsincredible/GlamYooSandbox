fd77d568656a4d74ea828fa74290947cb90d29d8
Kavita Gupta <kavita1392@gmail.com>
2015-05-24 20:32:16 +0530

    changed message from hello world to ghanta hello world.

--- src/com/glamyoo/sandbox/servlets/MyServlet.java
+++ src/com/glamyoo/sandbox/servlets/MyServlet.java
@@ -29,7 +29,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.write("Hello World!!");
+		out.write("Ghanta Hello World!!");
 	}
 
 	/**
